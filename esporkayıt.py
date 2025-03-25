ad006c32bb24c30b0cb73bba0e8647af325d3ddc
emirlkan01 <emirhankara368@gmail.com>
2025-03-25 20:09:48 +0300

    Add files via upload

---
+++ esporkayıt.py
@@ -0,0 +1,9 @@
+isim = input("isim giriniz:")
+soyisim = input("soyisim giriniz:")
+telefon = input("telefon numara giriniz:")
+yas = int(input("yaşınızı giriniz:"))
+if yas < 18:
+    print("18 yaşından küçükler espor kayıtları yapamaz")
+else:
+    print("kayıt başarılı")
+print("iyi günler")
